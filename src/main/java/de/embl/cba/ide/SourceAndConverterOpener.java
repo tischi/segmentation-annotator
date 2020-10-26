aeda008f8d9f21d99c900b3615fa469ab980254c
Christian Tischer <christian.tischer@embl.de>
2020-10-26 15:10:07 +0100

    WIP read segments and images from table

---
+++ src/main/java/de/embl/cba/ide/SourceAndConverterOpener.java
@@ -0,0 +1,39 @@
+package de.embl.cba.ide;
+
+import bdv.BigDataViewer;
+import bdv.tools.brightness.ConverterSetup;
+import bdv.viewer.SourceAndConverter;
+import ch.epfl.biop.bdv.bioformats.bioformatssource.BioFormatsBdvOpener;
+import ch.epfl.biop.bdv.bioformats.command.OpenFilesWithBigdataviewerBioformatsBridgeCommand;
+import ch.epfl.biop.bdv.bioformats.export.spimdata.BioFormatsConvertFilesToSpimData;
+import mpicbg.spim.data.generic.AbstractSpimData;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SourceAndConverterOpener
+{
+	public SourceAndConverterOpener()
+	{
+	}
+
+	public List< SourceAndConverter< ? > > open( final String imagePath )
+	{
+		OpenFilesWithBigdataviewerBioformatsBridgeCommand bridgeCommand = new OpenFilesWithBigdataviewerBioformatsBridgeCommand();
+		bridgeCommand.useBioFormatsCacheBlockSize = true;
+		BioFormatsBdvOpener opener = bridgeCommand.getOpener( new File( imagePath ) );
+
+		AbstractSpimData< ? > spimData = BioFormatsConvertFilesToSpimData.getSpimData( opener );
+		//BdvFunctions.show( spimData );
+
+		Map viewDescriptions = spimData.getSequenceDescription().getViewDescriptions();
+		List< ConverterSetup > converterSetups = new ArrayList<>();
+		List< SourceAndConverter< ? > > sourceAndConverters = new ArrayList<>();
+		BigDataViewer.initSetups( spimData, converterSetups, sourceAndConverters );
+
+		return sourceAndConverters;
+	}
+}
--- src/main/java/de/embl/cba/ide/TestRunner.java
+++ src/main/java/de/embl/cba/ide/TestRunner.java
@@ -1,23 +1,51 @@
 package de.embl.cba.ide;
 
+import bdv.util.BdvHandle;
+import bdv.viewer.SourceAndConverter;
 import de.embl.cba.tables.tablerow.TableRowImageSegment;
+import net.imagej.ImageJ;
+import sc.fiji.bdvpg.bdv.ScreenShotMaker;
+import sc.fiji.bdvpg.bdv.navigate.ViewerTransformAdjuster;
+import sc.fiji.bdvpg.bdv.projector.Projection;
+import sc.fiji.bdvpg.services.ISourceAndConverterService;
+import sc.fiji.bdvpg.services.SourceAndConverterServices;
+import sc.fiji.bdvpg.sourceandconverter.display.BrightnessAutoAdjuster;
+import sc.fiji.bdvpg.sourceandconverter.display.ProjectionModeChanger;
+import sc.fiji.bdvpg.spimdata.importer.SpimDataFromXmlImporter;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestRunner
 {
 	public static void main( String[] args )
 	{
+		ImageJ ij = new ImageJ();
+		ij.ui().showUI();
+
 		String rootDir = "/Users/tischer/Documents/joanna-zukowska-golgi-morphology/src/test/resources/image-data/";
 
 		TableRowImageSegmentsFromTableCreator creator = new TableRowImageSegmentsFromTableCreator( rootDir + "table.csv", false );
-
 		List< TableRowImageSegment > tableRows = creator.createTableRows();
 
 		ImagePathsFromTableRowsExtractor< TableRowImageSegment > extractor = new ImagePathsFromTableRowsExtractor<>( tableRows, rootDir, "relative_path_" );
-
 		List< String > imagePaths = extractor.extractImagePaths();
 
+		SourceAndConverterOpener opener = new SourceAndConverterOpener();
+		List< SourceAndConverter< ? > > sourceAndConverters = new ArrayList<>( );
+		for ( String imagePath : imagePaths )
+		{
+			List< SourceAndConverter< ? > > sources = opener.open( imagePath );
+			sources.forEach( s -> sourceAndConverters.add( s ) );
+		}
+
+		BdvHandle bdvHandle = SourceAndConverterServices.getSourceAndConverterDisplayService().getActiveBdv();
+
+		sourceAndConverters.forEach( sac -> {
+			SourceAndConverterServices.getSourceAndConverterDisplayService().show( bdvHandle, sac );
+			new ViewerTransformAdjuster( bdvHandle, sac ).run();
+			new BrightnessAutoAdjuster( sac, 0 ).run();
+		} );
 	}
+
 }
