55978c33cf51ce221db6822d2382a96f9ff81f9c
Christian Tischer <christian.tischer@embl.de>
2021-09-10 13:23:47 +0200

    Implement UI for multiple intensity images

--- src/test/java/run/RunOpenLabelMaskAndIntensityImagePlusCommand.java
+++ src/test/java/run/RunOpenLabelMaskAndIntensityImagePlusCommand.java
@@ -1,7 +1,8 @@
 package run;
 
-import de.embl.cba.segmentationannotator.command.ViewLabelMaskAndIntensityImagePlusCommand;
+import de.embl.cba.segmentationannotator.command.ViewLabelMaskAndMultipleIntensityImagePlusCommand;
 import ij.IJ;
+import ij.ImagePlus;
 import net.imagej.ImageJ;
 
 public class RunOpenLabelMaskAndIntensityImagePlusCommand
@@ -11,10 +12,10 @@ public class RunOpenLabelMaskAndIntensityImagePlusCommand
 		ImageJ ij = new ImageJ();
 		ij.ui().showUI();
 
-		final ViewLabelMaskAndIntensityImagePlusCommand command = new ViewLabelMaskAndIntensityImagePlusCommand();
+		final ViewLabelMaskAndMultipleIntensityImagePlusCommand command = new ViewLabelMaskAndMultipleIntensityImagePlusCommand();
 		String root = "/Users/tischer/Documents/segmentations-explorer/";
 		command.labelImage = IJ.openImage( root + "src/test/resources/label_image_3d.zip");
-		command.intensityImage = IJ.openImage( root + "src/test/resources/intensity_image_3d.zip");
+		command.intensityImages = new ImagePlus[]{ IJ.openImage( root + "src/test/resources/intensity_image_3d.zip" ), IJ.openImage( root + "src/test/resources/intensity_image_3d_downscaled.zip" ) };
 		command.run();
 	}
 }
